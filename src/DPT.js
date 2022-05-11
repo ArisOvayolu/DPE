61e967707a98c669ec8a96b92d96114707cd6788
B9029724 <a.ovayolu1@newcastle.ac.uk>
2022-05-11 23:38:42 +0100

    Further CSS and finsihed landing/about

--- src/DPT.js
+++ src/DPT.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
 import Box from "@mui/material/Box";
-import Footer from "./components/Footer";
 import YoutubeEmbed from "./components/YoutubeEmbed";
 import StatusBar from "./components/StatusBar";
+import Avatar from "@mui/material/Avatar";
+import HC from "./img/DPT/HiddenCosts.jpg";
+import MD from "./img/DPT/Misdirection.jpg";
+import PZ from "./img/DPT/PrivacyZuckering.jpg";
+import NC from "./img/DPT/NeedlessComplexity.jpg";
+import DA from "./img/DPT/DisguisedAds.jpg";
+import SB from "./img/DPT/SneakintoBasket.jpg";
+import CS from "./img/DPT/ConfirmShaming.jpg";
+import Divider from "@mui/material/Divider";
 
 const DPT = () => {
   return (
@@ -14,6 +22,7 @@ const DPT = () => {
         <h1
           style={{
             padding: "20px",
+            paddingTop: "50px",
             marginLeft: "700px",
             fontFamily: "Poppins",
           }}
@@ -21,53 +30,136 @@ const DPT = () => {
           {" "}
           Dark Pattern Techniques{" "}
         </h1>
-        <p
-          style={{
-            padding: "2px",
-            paddingLeft: "610px",
-            paddingRight: "475px",
-            fontFamily: "Century",
-            fontSize: 20,
+        <Divider light sx={{ padding: "10px" }} />
+        <Box
+          sx={{
+            paddingTop: "30px",
+            flexDirection: "row",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
           }}
         >
-          Lets dive into the different techniques that you will come across on
-          the web.
-        </p>
+          <Avatar alt={"ava"} src={HC} sx={{ width: 50, height: 50 }} />
+          <h2
+            style={{
+              fontFamily: "Poppins",
+              marginLeft: "30px",
+            }}
+          >
+            {" "}
+            Hidden Cost{" "}
+          </h2>
+        </Box>
+        <YoutubeEmbed embedId="BVHMLs_N7SQ" />
+        <Divider light />
 
-        <h2
-          style={{
-            paddingTop: "10px",
-            marginLeft: "890px",
-            fontFamily: "Poppins",
+        <Box
+          sx={{
+            flexDirection: "row",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
           }}
         >
-          {" "}
-          Hidden Cost{" "}
-        </h2>
+          <Avatar alt={"ava"} src={NC} sx={{ width: 50, height: 50 }} />
+          <h2 style={{ marginLeft: "30px", fontFamily: "Poppins" }}>
+            {" "}
+            Needless Complexity{" "}
+          </h2>
+        </Box>
         <YoutubeEmbed embedId="BVHMLs_N7SQ" />
+        <Divider light sx={{ padding: "10px" }} />
 
-        <h2 style={{ marginLeft: "845px", fontFamily: "Poppins" }}>
-          {" "}
-          Needless Complexity{" "}
-        </h2>
+        <Box
+          sx={{
+            flexDirection: "row",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
+          <Avatar alt={"ava"} src={MD} sx={{ width: 50, height: 50 }} />
+          <h2 style={{ marginLeft: "30px", fontFamily: "Poppins" }}>
+            {" "}
+            Misdirection{" "}
+          </h2>
+        </Box>
         <YoutubeEmbed embedId="BVHMLs_N7SQ" />
+        <Divider light sx={{ padding: "10px" }} />
 
-        <h2 style={{ marginLeft: "880px", fontFamily: "Poppins" }}>
-          {" "}
-          Misdirection{" "}
-        </h2>
+        <Box
+          sx={{
+            flexDirection: "row",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
+          <Avatar alt={"ava"} src={CS} sx={{ width: 50, height: 50 }} />
+          <h2 style={{ marginLeft: "30px", fontFamily: "Poppins" }}>
+            {" "}
+            Confirmshaming{" "}
+          </h2>
+        </Box>
         <YoutubeEmbed embedId="BVHMLs_N7SQ" />
+        <Divider light sx={{ padding: "10px" }} />
 
-        <h2 style={{ marginLeft: "870px", fontFamily: "Poppins" }}>
-          {" "}
-          Confirmshaming{" "}
-        </h2>
+        <Box
+          sx={{
+            flexDirection: "row",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
+          <Avatar alt={"ava"} src={PZ} sx={{ width: 50, height: 50 }} />
+          <h2 style={{ marginLeft: "30px", fontFamily: "Poppins" }}>
+            {" "}
+            Privacy Zuckering{" "}
+          </h2>
+        </Box>
         <YoutubeEmbed embedId="BVHMLs_N7SQ" />
+        <Divider light sx={{ padding: "10px" }} />
 
-        <h2 style={{ marginLeft: "855px", fontFamily: "Poppins" }}>
-          {" "}
-          Privacy Zuckering{" "}
-        </h2>
+        <Box
+          sx={{
+            flexDirection: "row",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
+          <Avatar alt={"ava"} src={DA} sx={{ width: 50, height: 50 }} />
+          <h2 style={{ marginLeft: "30px", fontFamily: "Poppins" }}>
+            {" "}
+            Disguised Ads{" "}
+          </h2>
+        </Box>
+        <YoutubeEmbed embedId="BVHMLs_N7SQ" />
+        <Divider light sx={{ padding: "10px" }} />
+
+        <Box
+          sx={{
+            flexDirection: "row",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
+          <Avatar alt={"ava"} src={SB} sx={{ width: 50, height: 50 }} />
+          <h2 style={{ marginLeft: "30px", fontFamily: "Poppins" }}>
+            {" "}
+            Sneak into Basket{" "}
+          </h2>
+        </Box>
         <YoutubeEmbed embedId="BVHMLs_N7SQ" />
       </Box>
     </>
