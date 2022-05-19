5f4ded6d6b9b4c44d9dc701b2679573a0d14c339
B9029724 <a.ovayolu1@newcastle.ac.uk>
2022-05-19 19:42:31 +0100

    tiny css change to landing

--- src/Landing.js
+++ src/Landing.js
@@ -320,7 +320,7 @@ const Landing = () => {
             fontSize: 19,
           }}
         >
-          <span style={{color: "black", fontSize:19, fontFamily:"Poppins"}}>Piqued your interest?</span> Let's have a more in depth look at the different
+          <span style={{color: "black", fontSize:21, fontFamily:"Poppins"}}>Piqued your interest?</span> Let's have a more in depth look at the different
           techniques.
         </p>
         <Button
